8c7def5b1fe54490c6fc761e8b15deca349f6f9f
CarterMcCall53 <me@cmccall.dev>
2022-05-12 19:22:27 -0600

    Shortened Code Removed World Variable

--- src/main/java/dev/cmccall/compactgrassplugin/Events.java
+++ src/main/java/dev/cmccall/compactgrassplugin/Events.java
@@ -8,13 +8,6 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class Events implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
-        var world = e.getPlayer().getWorld();
-        var x = e.getPlayer().getLocation().getX();
-        var y = e.getPlayer().getLocation().getY();
-        var z = e.getPlayer().getLocation().getZ();
-        int X = (int) x;
-        int Y = (int) y;
-        int Z = (int) z;
         if (e.getFrom().getBlockX() == e.getTo().getBlockX()) {
             if (e.getFrom().getBlockY() == e.getTo().getBlockY()) {
                 if (e.getFrom().getBlockZ() == e.getTo().getBlockZ()) {
@@ -25,9 +18,15 @@ public class Events implements Listener {
         if (e.getPlayer().isFlying()) {
             return;
         }
+        var x = e.getPlayer().getLocation().getX();
+        var y = e.getPlayer().getLocation().getY();
+        var z = e.getPlayer().getLocation().getZ();
+        int X = (int) x;
+        int Y = (int) y;
+        int Z = (int) z;
         for (int i = Y; i > -65; i--) {
-            if (world.getType(X, i, Z) == Material.GRASS) {
-                var loc = e.getPlayer().getLocation();
+            var loc = e.getPlayer().getLocation();
+            if (e.getPlayer().getWorld().getType(X, i, Z) == Material.GRASS) {
                 e.getPlayer().getWorld().setType(loc, Material.AIR);
                 e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.BLOCK_GRASS_BREAK, 1f, 1);
                 break;
